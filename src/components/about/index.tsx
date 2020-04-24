09da9417ae3eacea67743d02492610694e7b71ae
Horroon <horroona@gmail.com>
2020-04-24 11:09:08 +0500

    footer style changed

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -4,10 +4,10 @@ import Item from './item';
 import './style.scss';
 
 const Items: Array<ItemFace> = [
-    { title: "About", iconClass: "fa fa-users", desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
-    { title: "Mail", iconClass: "fa fa-envelope", desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
-    { title: "Contact", iconClass: "fa fa-address-book", desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
-    { title: "Gallery", iconClass: "fa fa-camera-retro", desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
+    { title: "About", iconClass: "fa fa-users", desc: "Social connection is important as it improves our physical, emotional, and mental health." },
+    { title: "Mail", iconClass: "fa fa-envelope", desc: "Take my affection at the outset of the email.You know, reading is one of the best habits of the life." },
+    { title: "Contact", iconClass: "fa fa-address-book", desc: "If you live to be 100, I hope I live to be 100 minus 1 day, so I never have to live without you." },
+    { title: "Gallery", iconClass: "fa fa-camera-retro", desc: "Photographs play an important role, they connect to past, they remind of people, places, and stories" },
 ]
 const AboutMain = (): JSX.Element => {
     return <div className="row hm-rw-2">
--- src/components/footer/itemColumn.tsx
+++ src/components/footer/itemColumn.tsx
@@ -4,8 +4,9 @@ import { itemFace } from './interface'
 const GiveColumn: React.FC<itemFace> = (props): JSX.Element => {
     return <div className="col-lg-3">
         <div>
-            <i className={`${props.iconClass} ftr-icn-cls`} />
-            <span className="ftr-titl-cls">{props.title}</span>
+            <div className="ftr-icn-ttl-dv">
+                <i className={`${props.iconClass} ftr-icn-cls`} />
+                <span className="ftr-titl-cls">{props.title}</span></div>
             <div className="ftr-pr-dv" dangerouslySetInnerHTML={{
                 __html: `<p>${props.desc}</p>`
             }}>
