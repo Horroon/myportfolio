08d55053c27e72144e31bc0dc6979ef36bb16b46
Horroon <horroona@gmail.com>
2020-04-28 10:25:08 +0500

    carousel code modified

--- src/components/p_slider/index.tsx
+++ src/components/p_slider/index.tsx
@@ -5,34 +5,29 @@ import Scene3 from '../../assets/s3.jpg';
 import Scene4 from '../../assets/s4.jpg';
 import './style.scss'
 
+const imgs = [
+  { url: Scene1 },
+  { url: Scene2 },
+  { url: Scene3 },
+  { url: Scene4 },
+]
+
 const P_Slider = () => {
-  const imgs = [
-    { url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Los_Angeles_with_Mount_Baldy.jpg/270px-Los_Angeles_with_Mount_Baldy.jpg" },
-    { url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Los_Angeles_with_Mount_Baldy.jpg/270px-Los_Angeles_with_Mount_Baldy.jpg" },
-    { url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Los_Angeles_with_Mount_Baldy.jpg/270px-Los_Angeles_with_Mount_Baldy.jpg" },
-  ]
+ 
   return <div id="demo" className="carousel slide" data-ride="carousel">
 
     <ul className="carousel-indicators crsl-dot">
-      <li data-target="#demo" data-slide-to="0" className="active"></li>
-      <li data-target="#demo" data-slide-to="1"></li>
-      <li data-target="#demo" data-slide-to="2"></li>
-      <li data-target="#demo" data-slide-to="3"></li>
+      {
+        imgs.map((item,i)=><li data-target="#demo" data-slide-to={`${i}`} className={`${i==0&&"active"}`}></li>)
+      }
     </ul>
 
     <div className="carousel-inner crsl-itm">
-      <div className="carousel-item active">
-        <img className="crsl-itm-img" src={Scene1} alt="Dhussi, Mianwali, Punjab" draggable={false} />
-      </div>
-      <div className="carousel-item">
-        <img className="crsl-itm-img" src={Scene2} alt="Dhussi, Mianwali, Punjab" draggable={false} />
-      </div>
-      <div className="carousel-item">
-        <img className="crsl-itm-img" src={Scene3} alt="Dhussi, Mianwali, Punjab" draggable={false} />
-      </div>
-      <div className="carousel-item">
-        <img className="crsl-itm-img" src={Scene4} alt="Dhussi, Mianwali, Punjab" draggable={false} />
-      </div>
+      {
+        imgs.map((item, i) => <div className={`carousel-item ${i == 0 && "active"}`}>
+          <img className="crsl-itm-img" src={item.url} alt="Dhussi, Mianwali, Punjab" draggable={false} />
+        </div>)
+      }
     </div>
 
     <a className="carousel-control-prev" href="#demo" data-slide="prev">
