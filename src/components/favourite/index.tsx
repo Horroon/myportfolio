b07a99a15ae6663d7b6e1dddb911ec6766443949
Horroon <horroona@gmail.com>
2020-05-01 10:42:40 +0500

    img updated

--- src/components/favourite/index.tsx
+++ src/components/favourite/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import TilesRow from './makefavoriteItem/makeTilesRow';
 import './style.scss'
+
+
+
 import LMB from '../../assets/books/LMB.jpg'
-import G1 from '../../assets/games/torrent.jpg';
-import SuperHero from '../../assets/games/superhero.jpg';
-import Fun from '../../assets/games/fun.jpg';
-import Fantasy from '../../assets/movies/fantasy.jpg'
+import tariq from '../../assets/books/tariqbz.jpg'
+import quaid from '../../assets/books/quaid.jpg'
+
+//movies
+import Dangal from '../../assets/movies/dangal.jpg';
+import Idiot from '../../assets/movies/idiot.jpg';
+import richardparker from '../../assets/movies/richardparker.jpg';
+//end movies
+
+//games
+import CMNDO from '../../assets/games/commando2.jpg';
+import NFS from '../../assets/games/NFS.jpg';
+import IGI from '../../assets/games/IGI.jpg';
+//games end
 
 interface ColumnFace {
     img: string,
@@ -17,9 +30,9 @@ interface RowFace {
     BigObject: Array<ColumnFace>,
 }
 
-const Books: Array<ColumnFace> = [{ title: "Muhammad's Life", img: LMB },{ title: "Muhammad's Life", img: LMB },{ title: "Muhammad's Life", img: LMB },{ title: "Muhammad's Life", img: LMB },{ title: "Muhammad's Life", img: LMB },{ title: "Muhammad's Life", img: LMB },{ title: "Muhammad's Life", img: LMB },{ title: "Muhammad's Life", img: LMB },]
-const Games: Array<ColumnFace> = [{ title: "Best Game", img: G1 },]
-const Movies: Array<ColumnFace> = [{ title: "Fantasy Movie", img: Fantasy },]
+const Books: Array<ColumnFace> = [{ title: "Muhammad's Life", img: LMB },{ title: "Tariq Bin Zayad", img: tariq },{ title: "Quaid e Azam", img: quaid }]
+const Games: Array<ColumnFace> = [{ title: "IGI", img: IGI },{ title: "Commando 2", img: CMNDO },{ title: "Need for speed", img: NFS },]
+const Movies: Array<ColumnFace> = [{ title: "3 Idiots", img: Idiot },{ title: "Dangal", img: Dangal },{ title: "Richard Parker", img: richardparker },]
 
 const MainItems: Array<RowFace> = [{ BigObject: Books, MainTitle: "Favorite Books" }, { BigObject: Movies, MainTitle: "Favorite Movies" }, { BigObject: Games, MainTitle: "Favorite Games" }]
 
