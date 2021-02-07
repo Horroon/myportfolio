7ba017add39632f4a1619ea337536bd542a220c9
horroon <horroona@gmail.com>
2021-02-07 18:39:27 +0500

    about page design completed

--- src/components/about/aboutme/index.js
+++ src/components/about/aboutme/index.js
@@ -6,11 +6,11 @@ import {store} from '../../../models/index';
 import {HeaderClasses,CommonProperties} from '../../../constants';
 import { Link } from 'react-router-dom';
 import {SideBar} from './sidebar';
+import {AboutBody} from './body';
 
 export const AboutMe = (props) => {
 
     const {sidebar,body} = props.aboutMe
-    debugger
     const changeMode = (e)=>{
         e.stopPropagation();
         const nextMode = sidebar.bgClass == CommonProperties.modes.dark ? CommonProperties.modes.white : CommonProperties.modes.dark
@@ -23,78 +23,7 @@ export const AboutMe = (props) => {
     return<div className="about-main">
             <div className="row">
                 <SideBar Profile={Profile} changeMode={changeMode} sidebar={sidebar} />
-                <div className={`col-lg-10 col-sm-12 about-description-side about_body_bg-${body.bgClass}`}>
-                    <div className="body-personal-info">
-                        <div className="body-personal-info-desc">
-                            <div className="body-personal-info-desc-bio">
-                                <h2>Haroon Rasheed</h2>
-                                <h4>Software Engineer</h4>
-                            </div>
-                            <div className="body-personal-info-desc-about">
-                            <p>I'm a software engineer specialised in frontend development and also work on backend development in node.js for scalable web apps. How can I help you in your project? Checkout my services page and resume.</p> 
-                            </div>
-                            <div className="body-personal-info-desc-btns">
-                                <div>
-                                    <button className="btn btn-success btn-sm"><i className="fa fa-cog" /><span> View services</span></button>
-                                    <button className="btn btn-success btn-sm"><i className="fa fa-user-circle-o" /> <span>View Resume</span></button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="body-personal-info-profile">
-                            <div className="body-personal-info-profile-img-parent">
-                                <img src={Profile} />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="body-personal-work">
-                    <div className="body-personal-work-whatIdo">
-                        <div className="body-personal-work-hdng">
-                            <h4>What I do</h4>
-                        </div>
-                        <div className="body-personal-work-about">
-                            <p>I
-                                 have more than 2 and half year experience building react apps and node.js backend. I also have more than 2 years experience in Graphql and prisma. Below there is a short summary of my skills, you can find out more about that in my resume.
-                            </p>
-                        </div>                
-                    </div>
-                    <div className="body-personal-work-mytechs">
-                        <div className="row">
-                            {[
-                                {
-                                    name:"Vanilla js",
-                                    desc: `Vanilla JS is a fast, lightweight, cross-platform framework
-                                    for building incredible, powerful JavaScript applications.`
-                                },
-                                {
-                                    name:"React, Angular and Vue",
-                                    desc: `These are three powerful javascript framework to build single page applications. All these framework using latest features of javascript. React is getting more popularity than other due to light weight due to virtual dom. React uses one-way binding.`
-                                },
-                                {
-                                    name:"Node.js",
-                                    desc: `Javascript run time environment that is used to create apies server in javscript. Express is being widely used in nodejs. Due to this tool javascript is able to render server side rendering. That is whay now javascript is not only being in frontend but also in backend side.`
-                                },
-                                {
-                                    name:"Github, Gitlab,buckit",
-                                    desc: `Very famous tools that are being used to maintain code. Teams can collaborate each other and test.
-                                    GitLab helps us collect performance metrics for both servers as well as our applications. It quickly lets us determine the impact of merging a particular branch and keep an eye on our production systems without ever leaving GitLab
-                                    Circle CI is an amazing tool for continous integration and auto deployment.`
-                                },
-                                ].map(skill=>{
-                                return<div className="col-lg-3 col-sm-1 skill-card w3-animate-bottom" >
-                                <div className="card">
-                                        <div><i class="fas fa-coffee" />
-                                        </div>
-                                       <div className="card-body">
-                                           <h5 className="card-title">{skill.name}</h5>
-                                           <p className="card-text">{skill.desc}</p>
-                                       </div>
-                                   </div>
-                             </div>
-                    })}
-                        </div>
-                    </div>
-                </div>
-                </div>
+                <AboutBody body={body} Profile={Profile} />
             </div>
     </div>
 }
--- src/components/about/aboutme/sidebar.jsx
+++ src/components/about/aboutme/sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 export const SideBar = ({sidebar,changeMode, Profile})=>{
     return<div className={`col-lg-2 col-sm-12 about-sidenav side-${sidebar.bgClass}`}>
     <div className="about-sidenav-main">
-        <div className="personal-info">
+        <div className="personal-info w3-animate-top">
             <div className="name-dv">
                 <h4>Haroon Rasheed</h4>
             </div>
@@ -21,7 +21,7 @@ export const SideBar = ({sidebar,changeMode, Profile})=>{
                 </div>
             </div>
         </div>
-        <div className="pages-section">
+        <div className="pages-section w3-animate-left">
             <div className="pages-section-1">
                 <div className="pages-section-1-1">
                     <div className="pages-items-parent">
@@ -51,7 +51,7 @@ export const SideBar = ({sidebar,changeMode, Profile})=>{
                 <div className="pages-section-hire"><button className="btn btn-md btn-success"><i  className="fa fa-paper-plane" /> Hire Me</button></div>
             </div>
         </div>
-        <div className="mode-section">
+        <div className="mode-section w3-animate-bottom">
             <div>
                 <h6><i className="fa fa-info-circle" /> <span>Dark mode</span></h6>
                 <div>
